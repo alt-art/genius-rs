cf5f8e4d75188ebc6ce1f56fc86c75ad9d9e4afd
altart <pedromendescraft@gmail.com>
2021-06-28 20:58:40 -0300

    Changing project structure for future optimizations

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,7 @@
 //! ```
 
 use reqwest::Client;
-
-pub mod search;
-use search::SearchResponse;
+use serde::{Deserialize};
 
 #[cfg(test)]
 mod tests {
@@ -58,4 +56,73 @@ impl Genius {
         let result: SearchResponse = serde_json::from_str(&res.as_str()).unwrap();
         Ok(result)
     }
+}
+
+
+#[derive(Deserialize, Debug)]
+pub struct SearchResponse {
+    pub meta: Meta,
+    pub response: Hits
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Meta {
+    pub status: u32
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Hits {
+    pub hits: Vec<Hit>
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Hit {
+    pub highlights: [String;0],
+    pub index: String,
+    pub r#type: String,
+    pub result: SongSearch
+}
+
+#[derive(Deserialize, Debug)]
+pub struct SongSearch {
+    pub annotation_count: u32,
+    pub api_path: String,
+    pub full_title: String,
+    pub header_image_thumbnail_url: String,
+    pub header_image_url: String,
+    pub id: u32,
+    pub lyrics_owner_id: u32,
+    pub lyrics_state: String,
+    pub path: String,
+    pub pyongs_count: u32,
+    pub song_art_image_thumbnail_url: String,
+    pub song_art_image_url: String,
+    pub song_art_primary_color: Option<String>,
+    pub song_art_secondary_color: Option<String>,
+    pub song_art_text_color: Option<String>,
+    pub stats: SongStatus,
+    pub title: String,
+    pub title_with_featured: String,
+    pub url: String,
+    pub primary_artist: Artist
+}
+
+#[derive(Deserialize, Debug)]
+pub struct SongStatus {
+    pub unreviewed_annotations: u32,
+    pub hot: bool,
+    pub pageviews: u32
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Artist {
+    pub api_path: String,
+    pub header_image_url: String,
+    pub id: u32,
+    pub image_url: String,
+    pub is_meme_verified: bool,
+    pub is_verified: bool,
+    pub name: String,
+    pub url: String,
+    pub iq: Option<u32>,
 }
